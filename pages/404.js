dcaa05f04eab511f2f9f8444f9e91ff154e0d5b3
Eleshinlomo <109764063+eleshinlomo@users.noreply.github.com>
2022-12-13 22:56:26 -0500

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,37 @@
+import {useEffect, useState} from 'react'
+import {useRouter} from 'next/router'
+
+
+
+
+
 const Notfound = () => {
-  return (
 
-    <div>
+    const [count, setCount] = useState(0)
+  
 
-    Ooops! You Fell in a ditch. I see you are on the wrong page but HANG ON... I will help you out really quick.
-    
-    </div>
-  )
-}
+const counter = useEffect(()=>{
 
-export default Notfound
+  setTimeout(()=>{
+    setCount((count)=>count + 1)
+  }, 1000)
+  if (count === 5) {
+    goToContract()
+  }
+
+},)
+const router = useRouter()
+const goToContract = ()=>router.push('/')
+
+    return (
+  
+      <div>
+  
+      Ooops! You Fell in a ditch. You are on the wrong page but HANG ON... I will help you out really quick.
+      <p class='text-blue text-4xl'>You will be redirected in 5secs {count}.</p>
+      
+      </div>
+    )
+  }
+  
+  export default Notfound
