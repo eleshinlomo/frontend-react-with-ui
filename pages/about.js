d6aa36b7a3801bee7fae13757823dc7aacec033c
Eleshinlomo <109764063+eleshinlomo@users.noreply.github.com>
2022-12-14 05:30:22 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,6 @@
-import React from 'react'
-
 const about = () => {
   return (
-    <div class='mx-auto sm:text-lg md:text-3xl lg:text-3xl text-white font-bold mt-20  text-center  py-5 bg-gradient-to-b from-orange-400 via-blue-500 to-zinc-500 h-screen rounded-full'>
+    <div class='mx-auto sm:text-lg md:text-3xl lg:text-3xl text-white font-bold mt-20  text-center  py-5 bg-gradient-to-b from-orange-400 via-green-700 to-zinc-500 h-screen rounded-full'>
     <ul class='mx-auto text-center mt-52 space-y-6 font-bold'>
         <li>Tech used for this project are:</li>
         <li class='hover:text-blue'>Next Js(React)</li>
