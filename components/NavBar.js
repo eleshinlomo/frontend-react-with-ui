6ab0e072b4d0913bbb4d7f395865242b44b2f723
Eleshinlomo <109764063+eleshinlomo@users.noreply.github.com>
2022-12-13 23:01:53 -0500

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,18 +1,18 @@
+
 import {useRouter} from 'next/router'
 
 
 const NavBar = () => {
 
   const router = useRouter()
-  const home = ()=>router.push('/')
+  const home = ()=>router.push('/portfolio')
   const lounge = ()=>router.push('/lounge')
-  const tech = ()=>router.push('/tech')
-  const nodejs = ()=>router.push('https://node-b46a.onrender.com/blogs')
+  const tech = ()=>router.push('/about')
   const github = ()=>router.push('https://github.com/eleshinlomo/frontend-react-with-ui')
   
 
   return (
-    <nav class='grid grid-cols-1'> 
+    <nav class='grid grid-cols-1 bg-red-500'> 
     
   <span className=' col-auto text-center py-8 space-x-4 items-center justify-center'>
 
@@ -20,7 +20,6 @@ const NavBar = () => {
             <button onClick={home} class='bg-black rounded-full px-4 py-4 text-center text-white hover:bg-blue-800 active:bg-white focus:bg-pink'>HOME</button>
             <button onClick={lounge} class='bg-black rounded-full px-4 py-4 text-center text-white hover:bg-blue-800 active:bg-white focus:bg-pink'>LOUNGE</button>
             <button onClick={tech} class='bg-black rounded-full px-4 py-4 text-center text-white hover:bg-blue-800 active:bg-white focus:bg-pink'>ABOUT</button>
-            <button onClick={nodejs} class='bg-black rounded-full px-4 py-4 text-center text-white hover:bg-blue-800 active:bg-white focus:bg-pink'>NODEJS</button>
             <button onClick={github} class='bg-black rounded-full px-4 py-4 text-center text-white hover:bg-blue-800 active:bg-white focus:bg-pink'>GITHUB</button>
             
             </span>
