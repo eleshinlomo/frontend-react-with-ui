1c2235c6182c8b6ee113b13442e07275dabe9d76
Eleshinlomo <109764063+eleshinlomo@users.noreply.github.com>
2022-12-13 22:53:51 -0500

    Update lounge.js

--- pages/lounge.js
+++ pages/lounge.js
@@ -1,29 +1,27 @@
 
 
-
-
 const lounge = () => {
 
 
-  return (
-    
-<div class='container relative mx-auto h-screen w-screen rounded-full mb-10 pb-10 mt-10  bg-gradient-to-l from-pink-900 via-orange-500 to-black'>
 
-<div class='row '>
-<div class='col space-y-8 py-80 text-1xl font-bold flex flex-col items-center justify-center text-center mx-0 space-x-6'>
-
-<h1> Well you came here early, I am still working backend so I can push more of my works here. Please check back soon.</h1>
-
-</div>
-
-
-
-</div>
-        
-
-    </div>
-  )
-}
-
-export default lounge
-
+    return (
+      
+  <div class='grid grid-cols-1 h-screen w-screen rounded-full bg-gradient-to-l from-pink-900 via-orange-500 to-black'>
+  
+  
+  <div class='columns-1 col-span-full space-y-8 py-80 text-1xl font-bold items-center justify-center text-center mx-0 space-x-6'>
+  
+  <h1> Well you came here early, I am still working backend so I can push more of my works here. Please check back soon.</h1>
+  
+  </div>
+  
+ 
+  
+  </div>
+          
+    )
+  }
+  
+  export default lounge
+  
+  
