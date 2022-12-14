46e3de30811eb30716876bf68506cca402093e3c
Eleshinlomo <109764063+eleshinlomo@users.noreply.github.com>
2022-12-14 04:47:19 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ const Home = () =>{
       </div>
       </div>
 
-      <div class=' grid grid-cols-6 gap-4 space-x-5 text-center bg-slate-800'>
+      <div class=' grid grid-cols-2 gap-4 space-x-5 text-center bg-slate-800'>
       
       
 
@@ -46,27 +46,19 @@ const Home = () =>{
       <Link href='/about' ><p class='place-items-end py-10 bg-red-600 text-white font-bold rounded-full text-lg'>See tech used for Project 1</p></Link>
       </div>
 
-      <div class='columns-2 col-span-full sm:text-sm md:text-3xl lg:text-4xl md:col-span-full lg:col-span-full  bg-gradient-to-r from-gray-900 via-gray-700 to-blue-900 text-white font-bold'>
-      
-      <img src={'/assets/Screenshot 2022-12-13 133913.png'} alt='website portfolio 2' class='content-start sm:h-screen sm:w-full'></img>
-      <p class='place-items-end md:py-38 lg:py-48 '>Made with Vanilla Javascript</p>
-      <Link href='https://landingpage5.netlify.app' ><p class='place-items-end py-10 mt-28 bg-gradient-to-r from-blue-800 to-gray-900 text-white font-bold rounded-full text-sm md:text-2xl lg:text-3xl'>See Project 2</p></Link>
-      </div>
 
-      <div class='columns-3 col-span-full mb-5 sm:text-sm md:text-2xl lg:text-3xl md:col-span-full lg:col-span-full bg-gradient-to-r from-blue-700 via-slate-800 to-blue-500  lg:space-x-8 text-white font-extralight '>
+      <div class='columns-2 col-span-full mb-5 sm:text-sm md:text-1xl lg:text-2xl md:col-span-full lg:col-span-full bg-gradient-to-r from-blue-700 via-slate-800 to-blue-500  lg:space-x-8 text-white font-extralight '>
       
       <img src={'/assets/nodejs.png'} alt='website portfolio 3' class='sm:h-screen sm:w-screen'></img>
       <h1 class=' py-content-end py-5 md:py-5 lg:py-28 sm:space-y-10 ml-auto'>This is a blog running on backend server</h1>
-      <p class='  py-content-end   space-x-12 space-y-4  md:space-x-32 lg:space-x-32 font-bold '>This wesbite was made with Node js, Express js and connected to MongoDB Database. Hosted on Render.</p>
+      <p class='  py-content-end   space-x-12 space-y-4  md:space-x-28 lg:space-x-28 font-bold '>This wesbite was made with Node js, Express js and connected to MongoDB Database. Hosted on Render.</p>
        <p class='animate-bounce'>Please wait for few seconds for the website to load after clicking</p>
-      <Link href='https://node-b46a.onrender.com/blogs ' ><h3 class=' content-around py-10 bg-gradient-to-r from-blue-800 to-gray-900 text-white font-bold rounded-full sm:text-sm md:text-2xl lg:text-3xl'>See Project 3</h3></Link>
+      <Link href='https://node-b46a.onrender.com/blogs ' ><h3 class=' content-around py-10 bg-gradient-to-r from-blue-800 to-gray-900 text-white font-bold rounded-full sm:text-sm md:text-2xl lg:text-3xl'>See Project 2</h3></Link>
       </div>
+
 
     </div>
   </div>
-)
-}
+)}
 
 export default Home
-
-
