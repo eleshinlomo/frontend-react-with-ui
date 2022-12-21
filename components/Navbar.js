7477289ce54cd1f5b141066327b6eacb13cfb592
Eleshinlomo <109764063+eleshinlomo@users.noreply.github.com>
2022-12-21 07:12:36 -0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,11 +5,11 @@ const Navbar = () => {
   return (
 
     <nav className=' bg-black bg-gradient-to-r md:from-black md:via-transparent md:to-indigo-700 lg:from-black lg:via-transparent lg:to-indigo-700  text-white font-bold  w-screen'>
-  <ul className=' md:flex lg:flex justify-center align-middle  sm:space-y-4 md:space-x-6 lg:space-x-6 py-6 mb-0 font-bold  '>
+  <ul className=' md:flex lg:flex justify-center align-middle  sm:space-y-16 md:space-x-6 lg:space-x-6 py-6 mb-0 font-bold md:space-y-4 lg:space-y-4  '>
     <Link href='/' style={{ textDecoration: 'none' }}><li className='mt-4 hover:text-orange-500 focus:text-yellow-400 text-white'>HOME</li></Link>
     <Link href='#'style={{ textDecoration: 'none' }}><li className='hover:text-orange-500 text-white'>ABOUT</li></Link>
     <Link href='#'style={{ textDecoration: 'none' }}><li className='hover:text-orange-500 text-white'>GET IN TOUCH</li></Link>
-    <Link href='#'style={{ textDecoration: 'none' }}><li className='hover:text-orange-500 text-white'>GITHUB</li></Link>
+    <Link href='https://github.com/eleshinlomo/frontend-react-with-ui'style={{ textDecoration: 'none' }}><li className='hover:text-orange-500 text-white'>GITHUB</li></Link>
   </ul>
 </nav>
 
