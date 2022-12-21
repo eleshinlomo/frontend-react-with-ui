0b4a646839bd60c9606b0a9c797eefd1637a8ec2
Eleshinlomo <109764063+eleshinlomo@users.noreply.github.com>
2022-12-21 06:59:19 -0500

    Update Navbar.js
    Navbar component modified.

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,12 +4,12 @@ import Link  from 'next/link'
 const Navbar = () => {
   return (
 
-    <nav className='bg bg-gradient-to-r from-black via-transparent to-indigo-700  text-white font-bold  w-screen'>
-  <ul className=' md:flex lg:flex justify-center align-middle  sm:space-y-4 md:space-x-6 lg:space-x-6 py-6 mb-0 font-bold '>
-    <Link href='/' style={{ textDecoration: 'none' }}><li className='mt-4 hover:text-orange-500 focus:text-yellow-400'>HOME</li></Link>
-    <Link href='#'style={{ textDecoration: 'none' }}><li className='hover:text-orange-500'>ABOUT</li></Link>
-    <Link href='#'style={{ textDecoration: 'none' }}><li className='hover:text-orange-500'>GET IN TOUCH</li></Link>
-    <Link href='https://github.com/eleshinlomo/frontend-react-with-ui'style={{ textDecoration: 'none' }}><li className='hover:text-orange-500'>GITHUB</li></Link>
+    <nav className=' bg-black bg-gradient-to-r md:from-black md:via-transparent md:to-indigo-700 lg:from-black lg:via-transparent lg:to-indigo-700  text-white font-bold  w-screen'>
+  <ul className=' md:flex lg:flex justify-center align-middle  sm:space-y-4 md:space-x-6 lg:space-x-6 py-6 mb-0 font-bold  '>
+    <Link href='/' style={{ textDecoration: 'none' }}><li className='mt-4 hover:text-orange-500 focus:text-yellow-400 text-white'>HOME</li></Link>
+    <Link href='#'style={{ textDecoration: 'none' }}><li className='hover:text-orange-500 text-white'>ABOUT</li></Link>
+    <Link href='#'style={{ textDecoration: 'none' }}><li className='hover:text-orange-500 text-white'>GET IN TOUCH</li></Link>
+    <Link href='#'style={{ textDecoration: 'none' }}><li className='hover:text-orange-500 text-white'>GITHUB</li></Link>
   </ul>
 </nav>
 
