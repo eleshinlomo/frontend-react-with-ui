9d5d58cde23cb7ac4e10acf6995ac1feab783df0
Eleshinlomo <109764063+eleshinlomo@users.noreply.github.com>
2022-12-22 10:38:17 -0500

    Create RandomNumberGenerator.js

---
+++ components/RandomNumberGenerator.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+const RandomNumberGenerator = ()=> {
+    
+  const [randomNumbers, setRandomNumbers] = useState([]);
+
+  
+  const generateRandomNumbers = () => {
+    const randomNumbers = [];
+    for (let i = 0; i < 3; i++) {
+      randomNumbers.push(Math.floor(Math.random()*100));
+    }
+    setRandomNumbers(randomNumbers);
+  };
+  
+
+
+
+
+  return (
+    <div>
+      <p className='flex justify-center'>Your random numbers are:</p>
+      <ul className='flex space-x-4 justify-center text-4xl font-bold -mt-4'>
+        {randomNumbers.map((number, index) => (
+          <li key={index} className=''>{number}</li>
+        ))}
+      </ul>
+      <div className='flex justify-center'>
+      <button onClick={generateRandomNumbers} className='text-center b ml-55 text-3xl -mt-2'>GENERATE</button>
+      </div>
+    </div>
+  );
+}
+
+export default RandomNumberGenerator;
