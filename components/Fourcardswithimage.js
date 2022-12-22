f9404665a1d931ea730c984874cd81014e83e112
Eleshinlomo <109764063+eleshinlomo@users.noreply.github.com>
2022-12-22 10:35:27 -0500

    Update Fourcardswithimage.js

--- components/Fourcardswithimage.js
+++ components/Fourcardswithimage.js
@@ -6,21 +6,21 @@ const Fourcardswithimage = () => {
         <section>
 <article>
   <span className='bg-radialpurple  sm:justify-center sm:align-middle md:space-x-8 lg-space-x-8 block  md:flex lg:flex w-full md:py-6 lg:py-6 '>
-    <div className='border hover:bg-blue-700  shadow-gray-900 justify-center align-middle bg-black bg-radialpurple w-full h-52 md:w-80 lg:w-80 overflow-hidden'>
+    <div className='border hover:bg-gray-900  shadow-gray-900 justify-center align-middle bg-black bg-radialpurple w-full h-52 md:w-80 lg:w-80 overflow-hidden'>
     <img src='https://images.unsplash.com/photo-1636819488524-1f019c4e1c44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80' className='justify-center  items-center w-full h-full' />
     
     </div>
 
-    <div className='border hover:bg-blue-700  shadow-gray-900 justify-center align-middle bg-black bg-radialpurple w-full h-52 md:w-80 lg:w-80 overflow-hidden'>
+    <div className='border hover:bg-gray-900  shadow-gray-900 justify-center align-middle bg-black bg-radialpurple w-full h-52 md:w-80 lg:w-80 overflow-hidden'>
     <img src='https://images.unsplash.com/photo-1614680376573-df3480f0c6ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80' className='justify-center items-center h-full w-full ' />
     </div>
 
-    <div className='border hover:bg-blue-700  shadow-gray-900 justify-center align-middle bg-black bg-radialpurple w-full h-52 md:w-80 lg:w-80 overflow-hidden'>
+    <div className='border hover:bg-gray-900 shadow-gray-900 justify-center align-middle bg-black bg-radialpurple w-full h-52 md:w-80 lg:w-80 overflow-hidden'>
     <img src='https://images.unsplash.com/photo-1611162617213-7d7a39e9b1d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80' className=' justify-center  items-center h-full w-full ' />
     
     </div>
     
-    <div className='border hover:bg-blue-700  shadow-gray-900 justify-center align-middle bg-black bg-radialpurple w-full h-52 md:w-80 lg:w-80 overflow-hidden'>
+    <div className='border hover:bg-gray-900  shadow-gray-900 justify-center align-middle bg-black bg-radialpurple w-full h-52 md:w-80 lg:w-80 overflow-hidden'>
     <img src='https://images.unsplash.com/photo-1633409361618-c73427e4e206?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80' className=' justify-center  items-center h-full w-full object-fill bg-cover' />
     
     </div>
